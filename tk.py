80ec3ac172c21cab6ea20ed4697d5763c2645775
jaybo-6 <jayshree.ishu@gmail.com>
2023-10-31 14:33:59 +0530

    start button, login pwd

---
+++ tk.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+
+def start_button_clicked():
+    login = login_entry.get()
+    password = password_entry.get()
+    # You can perform actions with login and password here
+    print("Login:", login)
+    print("Password:", password)
+
+# Create the main window
+root = tk.Tk()
+root.title("Login Form")
+
+# Create a label for the Login field
+login_label = tk.Label(root, text="Login:")
+login_label.pack()
+
+# Create an Entry field for Login
+login_entry = tk.Entry(root)
+login_entry.pack()
+
+# Create a label for the Password field
+password_label = tk.Label(root, text="Password:")
+password_label.pack()
+
+# Create an Entry field for Password
+password_entry = tk.Entry(root, show="*")  # Use 'show' option to hide the password
+password_entry.pack()
+
+# Create a Start button
+start_button = tk.Button(root, text="Start", command=start_button_clicked)
+start_button.pack()
+
+# Run the GUI application
+root.mainloop()
